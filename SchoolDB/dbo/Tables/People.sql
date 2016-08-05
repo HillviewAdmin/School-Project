2b1d6cc5daad03e70fc0528a85d37e48f3a1048f
Sochima <Sochima@SOCHIMA-PC>
2016-08-05 00:09:57 -0400

    Changed password data code

--- SchoolDB/dbo/Tables/People.sql
+++ SchoolDB/dbo/Tables/People.sql
@@ -6,24 +6,24 @@
     [MiddleName]   VARCHAR (35)   NOT NULL,
     [LastName]     VARCHAR (50)   NOT NULL,
 	[LoginID]      VARCHAR (50)   NOT NULL,
-	[Password]     VARCHAR (50)   NOT NULL,
+	[Password]     BINARY  (64)   NOT NULL,
     [CreatedBy]    VARCHAR (50)   NOT NULL,
     [DOB]          DATE           NOT NULL,
     [Address1]     VARCHAR (100)  NOT NULL,
-    [Address2]     VARCHAR (100) NULL,
+    [Address2]     VARCHAR (100)  NULL,
     [City]         VARCHAR (30)   NOT NULL,
     [State]        VARCHAR (50)   NOT NULL,
-    [PostalCode]   VARCHAR (10)    NULL,
+    [PostalCode]   VARCHAR (10)   NULL,
     [Country]      VARCHAR (35)   NULL,
     [EmailAddress] VARCHAR (50)   NULL,
     [Phone]        INT            NULL,
     [ParentID]     INT            NOT NULL,
     [CreateDate]   DATETIME       NOT NULL DEFAULT Getdate(),
-    [UpdatedBy]    INT   NOT NULL,
+    [UpdatedBy]    INT            NOT NULL,
     [UpdateDate]   DATETIME       NOT NULL DEFAULT Getdate(),
     CONSTRAINT [PK_People] PRIMARY KEY CLUSTERED ([PeopleID] ASC),
     FOREIGN KEY ([PeopleTypeID]) REFERENCES [dbo].[PeopleType] ([PeopleTypeID]),
     FOREIGN KEY ([UserTypeID]) REFERENCES [dbo].[UserType] ([UserTypeID])
 );
-
+   
 
