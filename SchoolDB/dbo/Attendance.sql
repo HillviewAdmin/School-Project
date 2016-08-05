b19d36f2e1a70dd762654fb08deaf83b3ab42065
HillviewAdmin <admin@hillviewgroupinc.com>
2016-08-05 18:41:31 -0400

    Added Attendance Table

---
+++ SchoolDB/dbo/Attendance.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Attendance]
+(
+	[PeopleID] INT NOT NULL, 
+    [ClassID] INT NOT NULL, 
+    [DayDate] DATE NOT NULL, 
+    [TeacherID] INT NOT NULL, 
+    [CreatedBy] INT NOT NULL, 
+    [CreateDate] DATETIME NOT NULL DEFAULT Getdate(), 
+    [UpdatedBy] INT NOT NULL, 
+    [UpdateDate] DATETIME NOT NULL DEFAULT Getdate(),
+	CONSTRAINT PK_Attendance PRIMARY KEY (PeopleID,ClassID,[DayDate])
+)
