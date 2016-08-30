67df69988203fe7346633ba78776a526df6a5892
Denis <denisuzoanya@gmail.com>
2016-08-29 21:37:54 -0500

    Update  the datatype for ClassID and added a stored procedure.

---
+++ SchoolDB/dbo/Stored Procedures/uspAddNewClass.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROC dbo.uspAddNewClass
+@pClassID NCHAR(10),
+@pClassName NVARCHAR(25)
+
+AS
+  BEGIN
+  SET NOCOUNT ON
+  DECLARE @Today DATETIME = GETDATE(),
+		  @Operator INT = 10000
+
+INSERT INTO Class (ClassID,ClassName, CreatedBy, CreateDate, UpdateBy, UpdateDate)
+VALUES (@pClassID, @pClassName, @Operator, @Today, @Operator, @Today)
+
+END
